986f08c5fc104e0707a7af409b25c4881e97faf7
Nikita Solovey <solovei292000@gmail.com>
2025-04-11 14:50:47 +0300

    end commit

---
+++ cultural_api/schema.py
@@ -0,0 +1,15 @@
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Lost Heritage API",
+        default_version='v1',
+        description="API для получения данных о культурных объектах, находящихся в розыске",
+        contact=openapi.Contact(email="support@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
--- cultural_api/urls.py
+++ cultural_api/urls.py
@@ -1,22 +1,17 @@
-"""cultural_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from .schema import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('lost_heritage.urls')),
+    path('swagger/',
+         schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/',
+         schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
+    path('swagger.json',
+         schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
 ]
